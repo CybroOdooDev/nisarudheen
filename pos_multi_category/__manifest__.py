e5e3ec9f939473268811b26e6f497d3755e91653
Nisar <nisardevala62@gmail.com>
2023-05-25 18:41:19 +0530

    [INIT]Pos_multi_categ

---
+++ pos_multi_category/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Pos Multi Category',
+    'version': '16.0.1.0.0',
+    'category': 'Point of sale',
+    'description': 'Select multiple category in point of sale',
+    'summary': 'Select multiple category in point of sale',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'point_of_sale'],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_multi_category/static/src/js/product_screen.js',
+        ]
+    },
+    'images': ['pos_multi_category/static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ pos_multi_category/doc/RELEASE_NOTE.md
@@ -0,0 +1,6 @@
+## Module <pos_multi_category>
+
+#### 25.05.2023
+#### Version 16.0.1.0.0
+#### ADD
+- Initial commit for Pos Multi Category
